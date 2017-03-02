c5443242cc45b5e7c88f0a3ea949b1499be53597
Luis Miguel Álvarez <elemikelo@gmail.com>
2017-03-02 21:41:50 +0100

    Agregamos campos 'owner y 'assignee al modelo Task y generamos migraciones

--- src/tasks/models.py
+++ src/tasks/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Task(models.Model):
@@ -12,10 +13,13 @@ class Task(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     status = models.CharField(max_length=3, default=PENDING, choices=STATUSES) # Pendiente, hecha
-    time_estimated = models.IntegerField(null=True)
-    deadline = models.DateField(null=True)
+    time_estimated = models.IntegerField(blank=True, null=True)
+    deadline = models.DateField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True) # automaticamente añada la fecha de creación
     modified_at = models.DateTimeField(auto_now=True) # automaticamente actualiza la fecha al guardar
+    owner = models.ForeignKey(User, related_name="owned_tasks") # crea usuario
+    assignee = models.ForeignKey(User, related_name="assigned_tasks", null=True, default=None) # asignada a alguien
+
 
     def __str__(self):
         return self.name # para que salga el nombre de la tarea, como toString()
