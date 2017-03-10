870fba5fea552eb8a5193a5b0b7954d7ea3596ef
Luis Miguel Álvarez <elemikelo@gmail.com>
2017-03-10 12:43:20 +0100

    Personalizacion del admin de django

--- src/tasks/api.py
+++ src/tasks/api.py
@@ -1,24 +1,15 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView
+
 
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
 
 
-class TasksAPI(APIView):
+class TasksAPI(ListCreateAPIView):
 
     """
     lists (GET) and creates (POST) Tasks
 
     """
-    def get(self, request):
-        """
-        Returns a list of tasks
-        :param request: httpRequest
-        :return: Response
-        """
-
-        tasks = Task.objects.all()
-
-        serializer = TaskSerializer(tasks, many=True)
-        return Response(serializer.data)
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
