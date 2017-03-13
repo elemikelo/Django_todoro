3784d8b2f52044abfcfee72513fab079efe7b60f
Luis Miguel Álvarez <elemikelo@gmail.com>
2017-03-13 13:23:26 +0100

    Search and ordering

--- src/tasks/api.py
+++ src/tasks/api.py
@@ -1,3 +1,4 @@
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
@@ -11,6 +12,10 @@ class TasksViewSet(ModelViewSet):
 
     queryset = Task.objects.all()
     permission_classes = (IsAuthenticated,)
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields= ("name", "description")
+    ordering_fields = ("id", "name", "owner", "assignee", "status", "created_at")
+
     def get_serializer_class(self):
         return TasksListSerializer if self.request.method == "list" else TaskSerializer
 
